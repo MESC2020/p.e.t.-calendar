604fc0e3e708c5a30d7d864b6d862220dba0e586
MESC2020 <71379157+MESC2020@users.noreply.github.com>
2022-03-04 17:12:20 -0800

    re-created skeleton
    react - electron - tailwind project

---
+++ electron/preload.ts
@@ -0,0 +1,8 @@
+//middleware of starting the web application and node.js itself
+
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    //We have some window and node.js access here to do stuff on initial load
+ });
+export {}
